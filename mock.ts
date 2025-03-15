54e5bf372f9f6fcc81d02ec5f5f028a287d7f2d5
João Jeefer da Silva Félix <joao.jeefer@gmail.com>
2025-03-15 15:57:41 -0300

    feat: add mocked list of values

---
+++ mock.ts
@@ -0,0 +1,274 @@
+import { Account } from "./src/context";
+
+export const accounstListMock: Account[] = [
+   {
+      code: "1",
+      name: "Receitas",
+      type: "Receita",
+      acceptEntries: false
+   },
+   {
+      code: "1.1",
+      name: "Taxa condominial",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.2",
+      name: "Reserva de dependência",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.3",
+      name: "Multas",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.4",
+      name: "Juros",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.5",
+      name: "Multa condominial",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.6",
+      name: "Água",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.7",
+      name: "Gás",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.8",
+      name: "Luz e energia",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.9",
+      name: "Fundo de reserva",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.10",
+      name: "Fundo de obras",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.11",
+      name: "Correção monetária",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.12",
+      name: "Transferência entre contas",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.13",
+      name: "Pagamento duplicado",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.14",
+      name: "Cobrança",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.15",
+      name: "Crédito",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.16",
+      name: "Água mineral",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.17",
+      name: "Estorno taxa de resgate",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.18",
+      name: "Acordo",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "1.19",
+      name: "Honorários",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "2",
+      name: "Despesas",
+      type: "Despesa",
+      acceptEntries: false
+   },
+   {
+      code: "2.1",
+      name: "Com pessoal",
+      type: "Despesa",
+      acceptEntries: false
+   },
+   {
+      code: "2.1.1",
+      name: "Salário",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.2",
+      name: "Adiantamento salarial",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.3",
+      name: "Hora extra",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.4",
+      name: "Férias",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.5",
+      name: "13º salário",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.6",
+      name: "Adiantamento 13º salário",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.7",
+      name: "Adicional de função",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.8",
+      name: "Aviso prévio",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.9",
+      name: "INSS",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.10",
+      name: "FGTS",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.11",
+      name: "PIS",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.12",
+      name: "Vale refeição",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.13",
+      name: "Vale transporte",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.14",
+      name: "Cesta básica",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "2.1.15",
+      name: "Acordo trabalhista",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "3",
+      name: "Despesas bancárias",
+      type: "Despesa",
+      acceptEntries: false
+   },
+   {
+      code: "3.1",
+      name: "Registro de boletos",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "3.2",
+      name: "Processamento de boletos",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "3.3",
+      name: "Registro e processamento de boletos",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "3.4",
+      name: "Resgates",
+      type: "Despesa",
+      acceptEntries: true
+   },
+   {
+      code: "4",
+      name: "Outras receitas",
+      type: "Receita",
+      acceptEntries: false
+   },
+   {
+      code: "4.1",
+      name: "Rendimento de poupança",
+      type: "Receita",
+      acceptEntries: true
+   },
+   {
+      code: "4.2",
+      name: "Rendimento de investimentos",
+      type: "Receita",
+      acceptEntries: true
+   }
+]
