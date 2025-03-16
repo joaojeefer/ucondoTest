e193d001be750d4141d01f85a26be2ce9ba46d45
João Jeefer da Silva Félix <joao.jeefer@gmail.com>
2025-03-16 10:56:26 -0300

    feat: add safe area view spacing

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,16 +3,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { AccountsList, DetailsAccount } from "./screens";
 import { RootStackParamList } from "./types";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export function Main() {
    return (
-      <NavigationContainer>
-         <Stack.Navigator>
-            <Stack.Screen name="Home" component={AccountsList} options={{ title: 'Plano de Contas' }} />
-            <Stack.Screen name="Details" component={DetailsAccount} />
-         </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+         <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+         <NavigationContainer>
+            <Stack.Navigator>
+               <Stack.Screen name="Home" component={AccountsList} options={{ title: 'Plano de Contas' }} />
+               <Stack.Screen name="Details" component={DetailsAccount} />
+            </Stack.Navigator>
+         </NavigationContainer>
+      </SafeAreaView>
    )
 }
