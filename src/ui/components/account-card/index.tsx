c4d2680a915507437cbb1648c024ce094b7589d1
João Jeefer da Silva Félix <joao.jeefer@gmail.com>
2025-03-16 18:05:28 -0300

    feat: add delete account modal to account -card

--- src/ui/components/account-card/index.tsx
+++ src/ui/components/account-card/index.tsx
@@ -1,23 +1,33 @@
 import Icon from "@react-native-vector-icons/fontawesome6";
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 import { AccountCardProps } from "./types";
 import { Metrics, Palette } from "../../../res";
+import { DeleteAccountModal } from "../delete-account-modal";
 
 export function AccountCard(props: AccountCardProps) {
    const { label, onDeletePress } = props
 
+   const [deleteAccountModalVisible, setDeleteAccountModalVisible] = useState(false)
+
    return (
-      <View style={styles.container}>
-         <Text style={styles.label} numberOfLines={2}>{label}</Text>
-         <Icon
-            name="trash"
-            color={Palette.secondary.light}
-            size={Metrics.font_size.x_small}
-            iconStyle="solid"
-            onPress={onDeletePress}
+      <>
+         <View style={styles.container}>
+            <Text style={styles.label} numberOfLines={2}>{label}</Text>
+            <Icon
+               name="trash"
+               color={Palette.secondary.light}
+               size={Metrics.font_size.x_small}
+               iconStyle="solid"
+               onPress={() => setDeleteAccountModalVisible(true)}
+            />
+         </View>
+         <DeleteAccountModal
+            isVisible={deleteAccountModalVisible}
+            onRequestClose={() => setDeleteAccountModalVisible(false)}
+            onPrimaryButtonPress={onDeletePress}
          />
-      </View>
+      </>
    )
 }
